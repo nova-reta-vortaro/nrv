250a92065f946e3d5dddf06832720d7f5a12ab7f
Bat <baptiste@gelez.xyz>
2018-02-10 16:24:29 +0100

    Basic search

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,78 @@ extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 extern crate markdown;
 
 use std::error::Error;
-use rocket_contrib::Template;
-use rocket::response::status::NotFound;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::PathBuf;
 use std::path::Path;
+use std::collections::HashMap;
+
+use rocket::State;
+use rocket_contrib::Template;
+use rocket::response::status::NotFound;
 use rocket::response::NamedFile;
+
+#[derive(Serialize, Deserialize)]
+struct Word {
+    word: String,
+    definition: String,
+    #[serde(rename = "see-also")]
+    see_also: Vec<String>,
+    translations: HashMap<String, Vec<String>>
+}
+
+impl Word {
+    fn from_file (filename: &str) -> std::io::Result<Word> {
+        let complete_path = format!("articles/{}.json", filename);
+        let path = Path::new(&complete_path);
+        let mut file = File::open(&path)?;
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        let mut word : Word = serde_json::from_str(contents.as_str()).unwrap();
+        word.definition = markdown::to_html(word.definition.as_str());
+        Ok(word)
+    }
+}
+
+struct Index {
+    words: Vec<String>
+}
+
+impl Index {
+    fn new () -> Index {
+        let paths = std::fs::read_dir("articles").unwrap();
+
+        Index {
+            words: paths.filter_map(|path| {
+                let file = String::from(format!("{}", path.unwrap().path().display()));
+                if file.ends_with(".json") {
+                    Some(file.replace("articles/", "").replace(".json", ""))
+                } else {
+                    None
+                }
+            }).collect()
+        }
+    }
+
+    fn filter (&self, search: &str) -> Vec<String> {
+        let mut res = Vec::new();
+
+        for word in self.words.clone() {
+            if word.contains(search) {
+                res.push(word.clone())
+            }
+        }
+
+        res
+    }
+}
 
 #[get("/static/<file..>")]
 fn static_files(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
@@ -43,28 +107,31 @@ struct SearchQuery {
 }
 
 #[get("/sercxu?<query>")]
-fn search_results(query: SearchQuery) -> Template {
+fn search_results(query: SearchQuery, index: State<Index>) -> Template {
     Template::render("search", &json!({
         "selected": "/sercxu",
         "query": query.demando,
-        "results": [
-            "test"
-        ]
+        "results": index.filter(&query.demando.unwrap_or("".to_string()))
     }))
 }
 
 #[get("/vorto/<vorto>")]
 fn word(vorto: String) -> Result<Template, NotFound<String>> {
-    let path = Path::new("articles/").join(format!("{}.md", vorto));
-    match markdown::file_to_html(&path) {
-        Err(why) => Err(NotFound(String::from(why.description()))),
-        Ok(html) => {
-            Ok(Template::render("word", &json!({
-                "title": vorto,
-                "content": html,
-            })))
-        }
+    match Word::from_file(&vorto.as_str()) {
+        Err(_) => match Word::from_file(&parse_x_notation(vorto).as_str()) {
+            Err(_) => Err(NotFound(String::from("Can't find requested file"))),
+            Ok(data) => Ok(Template::render("word", &serde_json::to_value(&data).unwrap()))
+        },
+        Ok(data) => Ok(Template::render("word", &serde_json::to_value(&data).unwrap()))
     }
+}
+
+fn parse_x_notation(text: String) -> String {
+    text.replace("cx", "ĉ")
+        .replace("gx", "ĝ")
+        .replace("hx", "ĥ")
+        .replace("jx", "ĵ")
+        .replace("sx", "ŝ")
 }
 
 fn main() {
@@ -75,6 +142,7 @@ fn main() {
         search_results,
         word
     ])
+    .manage(Index::new())
     .attach(Template::fairing())
     .launch();
 }
