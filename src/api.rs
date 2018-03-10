7100da7df1854ee568fe66792a3c3062afe8a38d
Bat <baptiste@gelez.xyz>
2018-03-10 16:04:05 +0100

    Examples are safe

--- src/api.rs
+++ src/api.rs
@@ -16,7 +16,7 @@ fn to_json<T> (data: Result<T>)-> Json where T: Serialize {
     }
 }
 
-#[get("/<vorto>")]
+#[get("/vorto/<vorto>")]
 fn word(vorto: String) -> Json {
     let w = utils::find_word(vorto);
     to_json(w)
